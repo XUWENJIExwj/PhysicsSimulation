2a4bc33729e70ad11a655aed311418cddaeb5b56
XUWENJIE <xwjslivermoon@gmail.com>
2022-05-16 01:26:25 +0900

    平面から平面のBallシミュレーション完了

---
+++ Assets/Kyo/Script/Ball.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public MyPhysics BallPhysics;
+    public float Power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            var angle = transform.localEulerAngles;
+            angle.y -= 1.0f;
+            transform.localEulerAngles = angle;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var angle = transform.localEulerAngles;
+            angle.y += 1.0f;
+            transform.localEulerAngles = angle;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            BallPhysics.AddForce(Power);
+        }
+    }
+}
